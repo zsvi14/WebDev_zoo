deb3b1bf8f34c9a051cc4baeba4bf2aefabbf971
siham <sihaml16.laghrib@gmail.com>
2024-11-20 10:21:40 +0100

    Update enclos.php

--- back/routes/enclos.php
+++ back/routes/enclos.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 include_once '../db/database.php';  // Assurez-vous que le chemin est correct
 
-$query = "SELECT e.nom, a.nom AS animal
+$query = "SELECT e.nom AS enclos_nom, a.nom AS animal
           FROM enclos e
           LEFT JOIN enclos_animaux ea ON e.id = ea.id_enclos
           LEFT JOIN animaux a ON ea.id_animal = a.id";
@@ -12,9 +12,12 @@ $result = $db->query($query);
 if ($result->num_rows > 0) {
     $enclos = [];
     while ($row = $result->fetch_assoc()) {
-        // Construire la structure des enclos avec leurs animaux
-        $enclos[$row['nom']]['nom'] = $row['nom'];
-        $enclos[$row['nom']]['animaux'][] = ['nom' => $row['animal']];
+        // Vérifier si l'enclos existe déjà dans le tableau
+        if (!isset($enclos[$row['enclos_nom']])) {
+            $enclos[$row['enclos_nom']] = ['nom' => $row['enclos_nom'], 'animaux' => []];
+        }
+        // Ajouter l'animal à l'enclos
+        $enclos[$row['enclos_nom']]['animaux'][] = ['nom' => $row['animal']];
     }
     echo json_encode(array_values($enclos));  // Renvoyer sous forme de tableau
 } else {
