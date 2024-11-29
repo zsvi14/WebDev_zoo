944042f8604b052cc2c76be2cde6d5a441f3a06b
siham <sihaml16.laghrib@gmail.com>
2024-11-29 11:22:06 +0100

    Update enclos.php

--- back/routes/enclos.php
+++ back/routes/enclos.php
@@ -2,11 +2,18 @@
 header('Content-Type: application/json');
 include_once '../db/database.php'; 
 
+// Requête SQL
 $query = "SELECT e.nom_enclos AS enclos_nom, e.nom_animal AS animal
           FROM enclos e
           LEFT JOIN animaux a ON e.nom_animal = a.nom";
 
+// Exécution de la requête
 $result = $db->query($query);
+
+// Vérification des erreurs SQL
+if (!$result) {
+    die('Erreur dans la requête : ' . $db->error);
+}
 
 if ($result->num_rows > 0) {
     $enclos = [];
