52e5ab5fc18174ac8205eb78fe00a262616a46bd
Aurore <aurore.jilliot@isen.yncrea.fr>
2024-11-29 09:06:36 +0100

    Update logout.php

--- back/connexion/logout.php
+++ back/connexion/logout.php
@@ -3,7 +3,9 @@
 session_start();
 // clear localStorage data car mes infos clients sont dans le local storage
 echo "<script>localStorage.clear();</script>";
+$sql = "SELECT id, name, email, role FROM users WHERE email = '$email'";
+session_destroy("SELECT id, name, email, role FROM users WHERE email = '$email'");
 
-header("Location: ../../front/HTML/connexion/index.html");
+header("Location: ../../../front/HTML/connexion/index.html");
 exit;
 ?>
